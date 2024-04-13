b31c0fcddc2c001aaccc194751ebb5cce284eaf2
ProgrammerJayT <theanthem8@gmail.com>
2024-04-13 21:25:36 +0200

    Add menu.jsx component to handle app bar menu items

---
+++ src/components/app-bar/menu.jsx
@@ -0,0 +1,13 @@
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+export const menuList = [
+  {
+    title: "Profile",
+    icon: <ManageAccountsIcon />,
+  },
+  {
+    title: "Logout",
+    icon: <LogoutIcon />,
+  },
+];
