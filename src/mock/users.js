5822563dc72ab129562142dddd72abc1d51fc631
ProgrammerJayT <theanthem8@gmail.com>
2024-04-14 00:43:45 +0200

    Add mock users data to src/mock/users.js

---
+++ src/mock/users.js
@@ -0,0 +1,15 @@
+export const users = [
+  {
+    id: 1,
+    name: "Kabelo",
+    surname: "Moobi",
+    email: "kabelo.moobi@mail.com",
+  },
+
+  {
+    id: 2,
+    name: "Ntsako",
+    surname: "Khoza",
+    email: "ntsako.khoza@mail.com",
+  },
+];
