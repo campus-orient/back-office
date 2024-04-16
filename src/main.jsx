d9293ca18a97eba336faeeca534f12ad95c63836
ProgrammerJayT <theanthem8@gmail.com>
2024-04-17 00:10:35 +0200

    Add headers function to main.jsx for API requests

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,12 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { headers } from "./constants/axios/config.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+headers();
