3f493b1a35dd621e9124ac0fa3a571cfe4992eaf
ProgrammerJayT <theanthem8@gmail.com>
2024-04-17 09:21:07 +0200

    Add AuthProvider component for authentication context

---
+++ src/context/auth/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { AuthContext } from "../declarations";
+
+const AuthProvider = ({ children }) => {
+  const [authCheck, setAuthCheck] = useState(false);
+  const [logged, setLogged] = useState(false);
+  const [user, setUser] = useState(null);
+
+  // Create the auth context value
+  const authContextValue = {
+    authCheck,
+    logged,
+    user,
+
+    setAuthCheck,
+    setLogged,
+    setUser,
+  };
+
+  return (
+    <AuthContext.Provider value={authContextValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthProvider;
