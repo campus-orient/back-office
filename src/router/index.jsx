f38d25d8644522b90a35d4f81794dcc497c5dbea
ProgrammerJayT <theanthem8@gmail.com>
2024-04-17 09:19:17 +0200

    Refactor router configuration to remove unused imports and add login route

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,20 +1,17 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AppLayout from "../layout";
 import HomeView from "../views/home";
-import AppBarComponent from "../components/app-bar";
-import { Box } from "@mui/material";
+import LoginView from "../views/auth/login";
 
 const RouteStack = () => {
   return (
     <>
-      <AppBarComponent />
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route index element={<HomeView />} />
-          {/* <Route path="blogs" element={<Blogs />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="*" element={<NoPage />} /> */}
+          <Route path="auth/login" element={<LoginView />} />
+          {/* <Route path="contact" element={<Contact />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
         </Route>
       </Routes>
     </>
