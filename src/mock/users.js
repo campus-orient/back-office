0fb4b24f1f8eee49e66de5bda7b54153b85d4970
ProgrammerJayT <theanthem8@gmail.com>
2024-04-17 10:38:53 +0200

    Remove commented out user objects in mock/users.js

--- src/mock/users.js
+++ src/mock/users.js
@@ -1,17 +1,16 @@
 export const users = [
-  {
-    id: 1,
-    name: "Kabelo",
-    surname: "Moobi",
-    email: "kabelo.moobi@mail.com",
-  },
-
-  {
-    id: 2,
-    name: "Ntsako",
-    surname: "Khoza",
-    email: "ntsako.khoza@mail.com",
-  },
+  // {
+  //   id: 1,
+  //   name: "Kabelo",
+  //   surname: "Moobi",
+  //   email: "kabelo.moobi@mail.com",
+  // },
+  // {
+  //   id: 2,
+  //   name: "Ntsako",
+  //   surname: "Khoza",
+  //   email: "ntsako.khoza@mail.com",
+  // },
 ];
 
 export const userTypes = ["student", "lecturer"];
