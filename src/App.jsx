fff41ae63662f795f052a533958283a1c4a366c5
ProgrammerJayT <theanthem8@gmail.com>
2024-04-21 02:03:55 +0200

    Add tanstack query package and update App.jsx to use QueryClientProvider and UsersProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 import RouteStack from "./router";
 import AuthProvider from "./context/auth";
+import UsersProvider from "./context/tanstack/query/users";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <AuthProvider>
-        <BrowserRouter>
-          <RouteStack />
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <UsersProvider>
+            <BrowserRouter>
+              <RouteStack />
+            </BrowserRouter>
+          </UsersProvider>
+        </QueryClientProvider>
       </AuthProvider>
     </>
   );
