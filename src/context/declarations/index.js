a7c0663a6c8b98da8e566163daa3e2a999cbec8c
ProgrammerJayT <theanthem8@gmail.com>
2024-04-21 02:10:38 +0200

    Add UsersContext to handle user-related data in context/declarations/index.js

--- src/context/declarations/index.js
+++ src/context/declarations/index.js
@@ -1,5 +1,7 @@
 import { createContext, useContext } from "react";
 
 export const AuthContext = createContext();
+export const UsersContext = createContext();
 
 export const useAuthContext = () => useContext(AuthContext);
+export const useUsersContext = () => useContext(UsersContext);
