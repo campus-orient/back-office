be2c641d0b428aa4b39a8cd4dc65a5b6363d1abb
ProgrammerJayT <theanthem8@gmail.com>
2024-04-21 02:05:08 +0200

    Update API_URL in axios config to use local development server

--- src/constants/axios/config.js
+++ src/constants/axios/config.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-export const API_URL = "https://campus-orient-api-83abdb28b436.herokuapp.com";
-
-const localToken = localStorage.getItem("token");
+export const API_URL = "http://127.0.0.1:8000";
+// export const API_URL = "https://campus-orient-api-83abdb28b436.herokuapp.com";
 
 export const headers = () => {
+  const localToken = localStorage.getItem("token");
+
   axios.defaults.headers.common["Authorization"] = `Bearer ${localToken}`;
   axios.defaults.headers.common["Accept"] = "application/json";
 };
